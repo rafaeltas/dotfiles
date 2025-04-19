f99a9e00574bf65acb1225e6c4ef2ca988572aa2
Rafael Tas <rafaeltas@gmail.com>
2025-04-18 21:20:05 -0300

    Add scripts nautilus and yazi

---
+++ nautilus/.local/share/scripts/Images/Converter_para_PNG.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+{
+
+  readarray FILENAME <<<"$(echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" | sed -e 's/\r//g')"
+
+  for file in "${FILENAME[@]}"; do
+    file=$(echo "$file" | tr -d $'\n')
+    convert "$file" "${file%.*}-converted.png"
+  done
+
+  notify-send "Deu certo!" "Os arquivos foram convertidos para PNG." --app-name="Transform" --icon="/home/raulcraveiro/.local/share/icons/custom/transform-symbolic.svg"
+
+}
